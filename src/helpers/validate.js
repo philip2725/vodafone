587f51edea009fd4e4a10b3f4a0a95b7143a043e
philip2725 <haertel.philip12@gmail.com>
2023-03-02 13:34:46 +0100

    registration UI + export validate functions

--- src/helpers/validate.js
+++ src/helpers/validate.js
@@ -1,4 +1,4 @@
-export function isEmail(email) {
+function isEmail(email) {
   if (!email) {
     return;
   }
@@ -10,4 +10,66 @@ export function isEmail(email) {
   } else {
     return false;
   }
+}
+
+export function validateLoginInput(data) {
+  const email = data.email;
+  const password = data.password;
+
+  let errors = {};
+
+  if (!isEmail(email)) {
+    errors["email"] = "Die angegebene E-Mail ist nicht korrekt";
+  }
+
+  if (!email) {
+    errors["email"] = "Deine E-Mail Adresse fehlt";
+  }
+
+  if (!password) {
+    errors["password"] = "Dein Passwort fehlt";
+  }
+
+  return {
+    valid: Object.keys(errors).length === 0,
+    errors: errors,
+  };
+}
+
+export function validateRegisterInput(data) {
+  const firstname = data.firstname;
+  const lastname = data.lastname;
+  const emailConfirm = data.emailConfirm;
+  const email = data.email;
+  const password = data.password;
+
+  let errors = {};
+
+  if (!isEmail(email)) {
+    errors["registerEmail"] = "Die angegebene E-Mail ist nicht korrekt";
+  }
+
+  if (!email) {
+    errors["registerEmail"] = "Deine E-Mail Adresse fehlt";
+  }
+
+  if (!emailConfirm || email !== emailConfirm) {
+    errors["emailConfirm"] = "Deine E-Mail Adressen stimmen nicht überein";
+  }
+
+  if (!firstname) {
+    errors["firstname"] = "Deine Vorname fehlt";
+  }
+  if (!lastname) {
+    errors["lastname"] = "Dein Nachname fehlt";
+  }
+
+  if (!password) {
+    errors["registerPassword"] = "Dein Passwort fehlt";
+  }
+
+  return {
+    valid: Object.keys(errors).length === 0,
+    errors: errors,
+  };
 }
--- src/pages/Authentication.js
+++ src/pages/Authentication.js
@@ -3,55 +3,57 @@ import { useState } from "react";
 import "./Authentication.css";
 import closeIcon from "../assets/close.png";
 
-import { isEmail } from "../helpers/validate";
+import { validateLoginInput, validateRegisterInput } from "../helpers/validate";
 import { TextInput } from "../components/Form";
 
 function Authentication(props) {
-  const [hasAccount, setHasAccount] = useState(false);
+  const [hasAccount, setHasAccount] = useState(true);
   const [loginData, setLoginData] = useState({});
+  const [registerData, setRegisterData] = useState({});
   const [inputState, setInputState] = useState({});
 
   function handleInputChange(event) {
     const name = event.target.name;
     const value = event.target.value;
 
-    setLoginData({
-      ...loginData,
-      [name]: value,
-    });
+    if (hasAccount) {
+      setLoginData({
+        ...loginData,
+        [name]: value,
+      });
+    } else {
+      setRegisterData({
+        ...registerData,
+        [name]: value,
+      });
+    }
   }
 
   function handleLogin(event) {
     event.preventDefault();
 
     //inputState data
-    let validInput = validateInput();
+    let { valid, errors } = validateLoginInput(loginData);
+    setInputState(errors);
 
-    if (!validInput) {
+    if (!valid) {
       return;
     }
 
     //login action
+    console.log("I am on a good way to be logged in ");
   }
 
-  function validateInput() {
-    const email = loginData.username;
-    const password = loginData.password;
-    let invalid = {};
+  function handleSignUp(event) {
+    event.preventDefault();
 
-    if (!isEmail(email)) {
-      invalid["email"] = "Die angegebene E-Mail ist nicht korrekt";
+    let { valid, errors } = validateRegisterInput(registerData);
+    setInputState(errors);
+    if (!valid) {
+      return;
     }
 
-    if (!email) {
-      invalid["email"] = "Deine E-Mail Adresse fehlt";
-    }
-
-    if (!password) {
-      invalid["password"] = "Dein Password fehlt";
-    }
-    setInputState(invalid);
-    return Object.keys(invalid).length === 0;
+    console.log("I am on a good way to be registered");
   }
 
   return (
@@ -68,33 +70,37 @@ function Authentication(props) {
 
         <div className="tab">
           <button
-            onClick={() => setHasAccount(false)}
-            className={hasAccount ? "tab-item" : "tab-item tab-item-active"}
+            onClick={() => setHasAccount(true)}
+            className={hasAccount ? "tab-item tab-item-active" : "tab-item"}
           >
             Anmelden
           </button>
           <button
-            onClick={() => setHasAccount(true)}
-            className={hasAccount ? "tab-item tab-item-active" : "tab-item"}
+            onClick={() => setHasAccount(false)}
+            className={hasAccount ? "tab-item" : "tab-item tab-item-active"}
           >
             Registrieren
           </button>
         </div>
 
+        <h2>
+          {hasAccount
+            ? "Login zu MeinVodafone"
+            : "Registrierung zu MeinVodafone"}
+        </h2>
+        <p>
+          Verwalte deine Verträge, Kundendaten und Geräteeinstellungen für
+          MeinVodafone
+        </p>
+
         <section
           className="login-section"
-          style={hasAccount ? { display: "none" } : { display: "flex" }}
+          style={hasAccount ? { display: "flex" } : { display: "none" }}
         >
-          <h2>Login zu MeinVodafone</h2>
-          <p>
-            Verwalte deine Verträge, Kundendaten und Geräteeinstellungen für
-            MeinVodafone
-          </p>
-
           <form className="form" onSubmit={handleLogin}>
             <TextInput
               label="E-Mail Adresse"
-              name="username"
+              name="email"
               type="email"
               onChange={handleInputChange}
               invalid={inputState.email}
@@ -116,27 +122,71 @@ function Authentication(props) {
           <button className="button-underline">Zugangsdaten vergessen</button>
           <button
             className="button-underline"
-            onClick={() => setHasAccount(true)}
+            onClick={() => setHasAccount(false)}
           >
             Registrieren
           </button>
         </section>
 
         <section
-          style={hasAccount ? { display: "block" } : { display: "none" }}
+          className="login-section"
+          style={hasAccount ? { display: "none" } : { display: "flex" }}
         >
-          <h2>Registrierung zurzeit nicht verfügbar!</h2>
-          <p style={{ maxWidth: "800px", margin: "0 auto" }}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-            mollitia, molestiae quas vel sint commodi repudiandae consequuntur
-            voluptatum laborum numquam blanditiis harum quisquam eius sed odit
-            fugiat iusto fuga praesentium optio, eaque rerum! Provident
-            similique accusantium nemo autem. Veritatis obcaecati tenetur iure
-            eius earum ut molestias architecto voluptate aliquam nihil, eveniet
-            aliquid culpa officia aut! Impedit sit sunt quaerat, odit, tenetur
-            error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
-            quia.
-          </p>
+          <form className="form" onSubmit={handleSignUp}>
+            <TextInput
+              label="Dein Vorname"
+              name="firstname"
+              type="text"
+              onChange={handleInputChange}
+              invalid={inputState.firstname}
+              errorMessage={inputState.firstname}
+            />
+
+            <TextInput
+              label="Dein Nachname"
+              name="lastname"
+              type="text"
+              onChange={handleInputChange}
+              invalid={inputState.lastname}
+              errorMessage={inputState.lastname}
+            />
+
+            <TextInput
+              label="E-Mail Adresse"
+              name="email"
+              type="email"
+              onChange={handleInputChange}
+              invalid={inputState.registerEmail}
+              errorMessage={inputState.registerEmail}
+            />
+
+            <TextInput
+              label="E-Mail Adresse bestätigen"
+              name="emailConfirm"
+              type="email"
+              onChange={handleInputChange}
+              invalid={inputState.emailConfirm}
+              errorMessage={inputState.emailConfirm}
+            />
+
+            <TextInput
+              label="Passwort"
+              name="password"
+              type="password"
+              onChange={handleInputChange}
+              invalid={inputState.registerPassword}
+              errorMessage={inputState.registerPassword}
+            />
+
+            <input type="submit" value="Registrieren" />
+          </form>
+
+          <button
+            className="button-underline"
+            onClick={() => setHasAccount(true)}
+          >
+            Anmelden
+          </button>
         </section>
       </div>
     </div>
